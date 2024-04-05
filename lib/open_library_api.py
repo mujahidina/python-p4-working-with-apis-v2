3842159b9036d8d10c1db64127709b6d287107db
mujahidina <mujahidabdi5070@gmail.com>
2024-04-05 16:38:23 +0300

    completed lab

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -43,6 +43,19 @@ class Search:
         response = requests.get(URL).json()
         response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
         return response_formatted
+    
+    def get_search_results_json(self):
+        search_term = "the lord of the rings"
+
+        search_term_formatted = search_term.replace(" ", "+")
+        fields = ["title", "author_name"]
+        fields_formatted = ",".join(fields)
+        limit = 1
+
+        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        print(URL)
+        response = requests.get(URL)
+        return response.json()
 
 
 # results = Search().get_search_results()
